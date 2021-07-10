2a39467f73b4e370591cfa62ad005b62e7878c99
Jonatã França <jonatafranca.eq@gmail.com>
2021-07-10 17:05:02 -0300

    add mobx

---
+++ lib/models/tarefa_controller.dart
@@ -0,0 +1,6 @@
+import 'package:mobx/mobx.dart';
+part 'tarefa_controller.g.dart';
+
+class TarefaController = _TarefaControllerBase with _$TarefaController;
+
+abstract class _TarefaControllerBase with Store {}
